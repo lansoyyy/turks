e24fd3cd2768b517bf197aa9a4104f281d161397
Lans12345 <olanalans12345@gmail.com>
2022-11-14 19:10:04 +0800

    fixed issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:turks/auth/login.dart';
 import 'package:turks/utils/firebase_options.dart';
-import 'package:turks/views/crew/crew_home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData(),
-      home: CrewHome(),
+      home: const LogInPage(),
     );
   }
 }
