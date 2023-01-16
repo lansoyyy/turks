316f0ca30c43c5b69e2d3bce2d1f833c39bb3035
Lans12345 <olanalans12345@gmail.com>
2023-01-16 17:55:30 +0800

    fixed minimal issue

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:turks/utils/firebase_options.dart';
-import 'package:turks/views/crew/crew_home.dart';
+import 'package:turks/views/admin/admin_home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData(),
-      home: CrewHome(),
+      home: const AdminHomePage(),
     );
   }
 }
