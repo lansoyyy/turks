5704353efc188fd82f9cf74e578b835bd6343084
Lans12345 <olanalans12345@gmail.com>
2023-04-17 20:00:00 +0800

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:turks/auth/login.dart';
 import 'package:turks/utils/firebase_options.dart';
 import 'package:turks/views/admin/admin_home_page.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData(),
-      home: const AdminHomePage(),
+      home: const LogInPage(),
     );
   }
 }
